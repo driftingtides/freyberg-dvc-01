63d3e89616ab25badcc736f932369f386a6a429e
Jeremy Bennett <jbennett@intera.com>
2025-08-07 14:29:36 +1200

    add dvc

--- scripts/workflow_exe.py
+++ scripts/workflow_exe.py
@@ -8,7 +8,7 @@ import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 def get_executables():
-    bindir = os.path.join("bin")
+    bindir = os.path.join("..", "bin")
     if os.path.exists(bindir):
         print("bin dir exists, skipping download")
         return
@@ -25,16 +25,6 @@ def get_executables():
         for fname in prefixes:
             f.write(fname + "\n")
     return
-
-def copy_executables_to_dir(ws,prefixes = ["mf6","zbud6","pestpp-glm"]):
-    # copy executables to dir
-    bindir = os.path.join("bin")
-    fnames = [f for f in os.listdir(bindir) if f.split(".")[0] in prefixes]
-    for f in fnames:
-        shutil.copy2(os.path.join(bindir,f),os.path.join(ws,f))
-    with open(os.path.join(ws, "exe.txt"), "w") as f:
-        for fname in fnames:
-            f.write(fname + "\n")
 
 
 if __name__ == "__main__":
